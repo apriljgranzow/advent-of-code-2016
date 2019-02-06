2345ccf249e7149c709d2cd8de3d993d67c3fed8
apriljgranzow <4971311+apriljgranzow@users.noreply.github.com>
2019-02-05 20:50:49 -0600

    Finished part one, part two not working

---
+++ 1/solution.py
@@ -0,0 +1,73 @@
+def turn(starting_direction,instruction):
+    '''Compute compass direction in degrees given a turn of left or right'''
+    if instruction == 'L':
+        return (starting_direction-90 + 360) % 360
+    else:
+        return (starting_direction+90 + 360) % 360 
+
+def travel(starting_position, direction, distance):
+    '''Given a starting position (distance from the origin), 
+    direction and distance, compute the final destination of the 
+    instruction'''
+    if direction == 0:      # North
+        return (starting_position[0]+distance,starting_position[1])
+    if direction == 90:     # East
+        return (starting_position[0],starting_position[1]+distance)
+    if direction == 180:    # South
+        return (starting_position[0]-distance,starting_position[1])
+    if direction == 270:    # West
+        return (starting_position[0],starting_position[1]-distance)
+
+
+def part_one(lst):
+    '''Start at the starting point and then compute the final destination
+    given a series of turns and distances.  Begin facing north.
+    North = 0 degrees
+    East = 90 degrees
+    South = 180 degrees
+    West = 270 degrees'''
+    position = (0,0) # rise, run
+    direction = 0
+    for instruction in lst:
+        direction = turn(direction, instruction[0])
+        distance = int(instruction[1:])
+        position = travel(position, direction, distance)
+    return position[0] + position[1]
+
+def visited_blocks(start,stop):
+    if start[0] == stop[0]:
+        distance = stop[1]-start[1]
+        return list(zip([start[0]]*(distance+1),range(distance+1)))
+    else:
+        distance = stop[0]-start[0]
+        return list(zip(range(distance+1),[start[1]]*(distance+1)))
+
+def get_all_visited_blocks(lst):
+    '''Return the first location visited twice.  Note this does not mean we
+    stopped at it necessarily, just that we walked past that block'''
+    position = (0,0) # rise, run
+    direction = 0
+    stops = set()
+    for instruction in lst:
+        direction = turn(direction, instruction[0])
+        distance = int(instruction[1:])
+        new_position = travel(position, direction, distance)
+        if new_position in stops:
+            import pdb; pdb.set_trace()
+            return (stops,new_position)
+        else:
+            stops.update(visited_blocks(position,new_position))
+            position = new_position
+    return (stops,position)
+
+def part_two(lst):
+    position = get_all_visited_blocks(lst)[1]
+    return position[0]+position[1]
+
+if __name__ == '__main__':
+    with open('input.txt') as file:
+        text = str(file.read())
+        inputs = text.split(', ')
+    example = ['R8', 'R4', 'R4', 'R8']
+    # print(part_one(inputs))
+    print(get_all_visited_blocks(example))
